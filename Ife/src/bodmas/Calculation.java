70b88a259e28241aaec3fe8f8a87d9200d7d84dc
tfaneye <tunde_eye@yahoo.com>
2019-05-08 20:20:02 +0100

    tf

---
+++ Ife/src/bodmas/Calculation.java
@@ -0,0 +1,17 @@
+package bodmas;
+
+public class Calculation {
+
+    private static int ans;
+    public static void main(String[] args){
+        int x =5;
+        ans = (9*x)- (2*7);
+        System.out.println("c. " + ans);
+
+        ans =(63/7)*(56/4);
+        System.out.println("\n d. " + ans);
+
+        ans = 12/2 * 3;
+        System.out.println("\n" +ans);
+    }
+}
